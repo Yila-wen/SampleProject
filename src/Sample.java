c3621c6a5151a14cf0e99ee3315620a51ab13bef
JiaXiangHuang <jhuang4836@bths.edu>
2022-09-20 19:47:58 -0400

    Added a println for favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Dracula");
+        System.out.println("Rice Cakes");
     }
 }
